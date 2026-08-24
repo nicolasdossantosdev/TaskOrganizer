76c3204ae581735524aa42ee379cd3b448157dd2
Nicolas Dos Santos <nicolas.dossantos.dev@gmail.com>
2026-08-24 16:05:50 +0700

    feat(app): wire up DI composition root and resolve MainWindow via ServiceProvider

--- TaskOrganizer/App.xaml.cs
+++ TaskOrganizer/App.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Configuration;
-using System.Data;
+using System.IO;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TaskOrganizer.Data;
+using TaskOrganizer.Services;
+using TaskOrganizer.ViewModels;
+using TaskOrganizer.Views;
 
 namespace TaskOrganizer;
 
@@ -9,5 +14,48 @@ namespace TaskOrganizer;
 /// </summary>
 public partial class App : Application
 {
-}
+    private ServiceProvider? _serviceProvider;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+
+        var contextFactory = _serviceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+        using (var dbContext = contextFactory.CreateDbContext())
+        {
+            dbContext.Database.Migrate();
+        }
+
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _serviceProvider?.Dispose();
+        base.OnExit(e);
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        var appDataFolder = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "TaskOrganizer");
+        Directory.CreateDirectory(appDataFolder);
+        var dbPath = Path.Combine(appDataFolder, "taskorganizer.db");
+
+        services.AddDbContextFactory<AppDbContext>(options =>
+            options.UseSqlite($"Data Source={dbPath}"));
+
+        services.AddSingleton<ITacheRepository, TacheRepository>();
+        services.AddSingleton<ITacheService, TacheService>();
+
+        services.AddTransient<CreateTacheViewModel>();
+        services.AddTransient<MainViewModel>();
+        services.AddTransient<MainWindow>();
+    }
+}
--- TaskOrganizer/Views/MainWindow.xaml.cs
+++ TaskOrganizer/Views/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 using System.Windows;
+using TaskOrganizer.ViewModels;
 
 namespace TaskOrganizer.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
+        DataContext = viewModel;
     }
 }
